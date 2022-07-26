feaeb1ece0fab600a9202f1349878d7fa51212d8
Keith Pickford <Keith@FunctionZero.com>
2022-07-26 14:48:19 +0100

    Slightly hacky fix for unexpected behaviour in iOS NavigationPage. The iOS app will now start up reliably.

--- TrilogyAvivaTest/TrilogyAvivaTest/App.xaml.cs
+++ TrilogyAvivaTest/TrilogyAvivaTest/App.xaml.cs
@@ -23,11 +23,18 @@ namespace TrilogyAvivaTest
 
             pageService.Init(this);
 
-            var thing = pageService.GetMvvmPage<HomePage, HomePageVm>(GetMvvmPageMode.Default);
-            _ = InitialiseTheVm(thing.viewModel, keyStore);
-            
             // This app will use a standard navigation stack.
-            MainPage = new NavigationPage(thing.page);
+
+            if (Device.RuntimePlatform == Device.iOS)
+                // iOS cannot initalise a NavigationPage without a root page provided up front.
+                // Providing a HomePage prevents the OnOwnerPagePushed event being raised in HomePageVm
+                // So I'm just jimmying in a dud page at the root and disabling the back-button in HomePage.xaml because this is just a demo.
+                MainPage = new NavigationPage(new ContentPage() { Content=new Label { Text="Don't look behind the curtain!"} });
+            else
+                MainPage = new NavigationPage();
+
+            // Present the HomePage, bound to a HomePageVm.
+            pageService.PushPageAsync<HomePage, HomePageVm>(async (vm) => await InitialiseTheVm(vm, keyStore));
         }
 
         private async Task InitialiseTheVm(HomePageVm vm, IKeyStore keyStore)
@@ -35,7 +42,7 @@ namespace TrilogyAvivaTest
             // Determine how many times the app has been launched from a cold-start and inform the vm.
             var result = await keyStore.ReadStringAsync(Constants.RunCountKey);
             int runCount;
-            if(result == null)
+            if (result == null)
             {
                 runCount = 1;
             }
@@ -45,7 +52,7 @@ namespace TrilogyAvivaTest
                     runCount = 1;
             }
 
-            await keyStore.WriteStringAsync(Constants.RunCountKey, (runCount+1).ToString());
+            await keyStore.WriteStringAsync(Constants.RunCountKey, (runCount + 1).ToString());
 
             vm.Init(runCount);
         }
