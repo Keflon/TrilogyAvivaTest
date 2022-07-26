b2742aea86c59da3e8d43206fe7cd604bb9b8c05
Keith Pickford <Keith@FunctionZero.com>
2022-07-26 09:39:13 +0100

    Added Android slash screen.

--- TrilogyAvivaTest/TrilogyAvivaTest.Android/MainActivity.cs
+++ TrilogyAvivaTest/TrilogyAvivaTest.Android/MainActivity.cs
@@ -8,7 +8,8 @@ using TrilogyAvivaTest.Bootstrap;
 
 namespace TrilogyAvivaTest.Droid
 {
-    [Activity(Label = "TrilogyAvivaTest", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    //[Activity(Label = "TrilogyAvivaTest", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "TrilogyAvivaTest", Icon = "@mipmap/icon", Theme = "@style/MainTheme", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
